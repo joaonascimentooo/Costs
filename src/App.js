033c9fae1961d4efe1bad543a6df3e27a184e695
joaonascimentooo <joaonascimento197@gmail.com>
2025-05-25 14:52:46 -0300

    Adicionando rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Company from './components/pages/Company.js';
+
 function App() {
   return (
-    <div className="App">
-    <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
