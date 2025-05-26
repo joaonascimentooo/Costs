72024054aab86315ae8a3d587611c5af8bcf16b6
joaonascimentooo <joaonascimento197@gmail.com>
2025-05-26 14:12:44 -0300

    Adicionando banco de dados Ficticio

--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,14 +1,40 @@
+import { useEffect, useState } from 'react';
 import styles from './ProjectForm.module.css';
 import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 
 function ProjectForm({btnText}){
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            setCategories(data);
+        })
+        .catch((error) => console.error('Error fetching categories:', error));
+    }, []);
+    console.log(categories);
     return(
         <form className={styles.form}>
-            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto!" />
-            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total!" />
-            <Select name="category_id" text="Seleciona a categoria"/>
+            <Input type="text" 
+            text="Nome do projeto" 
+            name="name" 
+            placeholder="Insira o nome do projeto!" />
+            <Input type="number" 
+            text="Orçamento do projeto" 
+            name="budget" 
+            placeholder="Insira o orçamento total!" />
+            <Select 
+            name="category_id" 
+            text="Seleciona a categoria"
+            options={categories}
+            />
             <SubmitButton text={btnText}/>
         </form>
     )
