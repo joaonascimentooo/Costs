2a8f9b4756bd9af20435869965328c31415971d9
joaonascimentooo <joaonascimento197@gmail.com>
2025-05-26 12:47:53 -0300

    Adicionando pagina de novo Projeto

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import ProjectForm from '../projects/ProjectForm'
+import styles from './NewProject.module.css'
+
 function NewProject(){
     return(
-        <h1>Novo Projeto</h1>
+        <div className={styles.newproject_container}>
+            <h1>Crie seu Projeto</h1>
+            <p>Crie seu projeto para depois adicionar os serviços!</p>
+            <ProjectForm />
+        </div>  
     )
 }
 export default NewProject
